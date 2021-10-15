86bb17d141bd6b9f8668357e0caf16cd27160bc6
Theo Ferguson <ferguson.theo@gmail.com>
2021-10-15 11:25:56 -0400

    commit tree 2465c3684a7910bc5bc7aa68267a500cc5420aa1

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,14 +1,13 @@
 class AuthorsController < ApplicationController
-  
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
   def show
     author = Author.find(params[:id])
-
     render json: author
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
   end
 
@@ -16,6 +15,10 @@ class AuthorsController < ApplicationController
   
   def author_params
     params.permit(:email, :name)
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: {errors: invalid.record.errors }, status: :unprocessable_entity
   end
   
 end
